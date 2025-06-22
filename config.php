9cb74d3a0d5f18dcb93c0cc4ec0ee069586fa119
raegun2 <148833221+raegun2@users.noreply.github.com>
2025-06-22 13:13:38 +1000

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,1 @@
-<?php
-return [
-  'DB_HOST' => '127.0.0.1',
-  'DB_USER' => 'onlineta_info',
-  'DB_PASS' => 'Dlforms00',
-  'DB_NAME' => 'onlineta_onlinetaxrefundtoday',
-  'CLICKSEND_USER' => 'excellentaccountant@outlook.com',
-  'CLICKSEND_KEY' => '72C2D3EC-A5F0-04B6-E744-96D1B73563B4',
-];
-?>
+
